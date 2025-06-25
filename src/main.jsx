1aaef01105c3534180806ffb2a98c091b2dc5547
KAMI <tendaankit@gmail.com>
2025-06-25 17:14:19 +0530

    Fix App import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // Corrected path
+import App from './app.jsx' // Corrected path
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
